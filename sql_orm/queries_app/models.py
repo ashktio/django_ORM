64d3ed1b523adff6386a1390a083774494d2f877
Ashenafi Teressa <ashenafi.teressa@gmail.com>
2021-07-27 21:52:38 -0700

    practice orm

---
+++ sql_orm/queries_app/models.py
@@ -0,0 +1,15 @@
+from re import M
+from django.db import models
+
+# Create your models here.
+
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
+
+    def __repr__(self):
+        return self.house
+        
+
